7ec0cc282a5f55f9225fc69b6613f47fe687e2e6
Suvam Debnath <debnathsuvam322@gmail.com>
2025-09-27 19:58:56 +0530

    created a UserDetailsService

--- src/main/java/com/suvam/security/service/MyUserDetailsService.java
+++ src/main/java/com/suvam/security/service/MyUserDetailsService.java
@@ -9,8 +9,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
-    @Autowired
-    private UserRepo repo;
+
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
